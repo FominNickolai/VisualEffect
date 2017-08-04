7d3cec1bbadddea26ff560d670677ba53975ca74
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-04 19:07:42 +0300

    Initial Commit

---
+++ VisualEffect/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  VisualEffect
+//
+//  Created by Simon Ng on 24/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
